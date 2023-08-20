93c1651e6ed3cd527d2f68598197591b2b9a141e
Rowa <jrowah@gmail.com>
2023-08-21 00:11:58 +0300

    create new app version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule IslandsEngine.MixProject do
     [
       app: :islands_engine,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
