4f337c5588877a1ce729b15f78748ecf85e8e855
Rowa <jrowah@gmail.com>
2023-12-19 12:25:39 +0300

    adds hits and misses on island coordinates

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -1,5 +1,5 @@
 defmodule IslandEngine.Guesses do
-  alias __MODULE__
+  alias IslandEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
@@ -7,4 +7,12 @@ defmodule IslandEngine.Guesses do
   # Elixir’s MapSet data structure guarantees that each member of the MapSet will be unique to cater for the fact that a specific guess can be done more than once. A guess already made will simply be ignored by MapSet. Returns a new guesses struct.
   @spec new() :: %IslandEngine.Guesses{hits: MapSet.t(), misses: MapSet.t()}
   def new(), do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate) do
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+  end
+
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate) do
+    update_in(guesses.miss, &MapSet.put(&1, coordinate))
+  end
 end
