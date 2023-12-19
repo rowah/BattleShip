5521be6dd29e3d2a09971ff82fbd894993f1c987
Rowa <jrowah@gmail.com>
2023-12-19 12:01:39 +0300

    creates new island coordinates

--- lib/guesses.ex
+++ lib/guesses.ex
@@ -4,5 +4,7 @@ defmodule IslandEngine.Guesses do
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
 
+  # Elixir’s MapSet data structure guarantees that each member of the MapSet will be unique to cater for the fact that a specific guess can be done more than once. A guess already made will simply be ignored by MapSet. Returns a new guesses struct.
+  @spec new() :: %IslandEngine.Guesses{hits: MapSet.t(), misses: MapSet.t()}
   def new(), do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
 end
--- lib/island.ex
+++ lib/island.ex
@@ -4,6 +4,36 @@ defmodule IslandEngine.Island do
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
-  def new(), do:
-    %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+  def new(type, %Coordinate{} = upper_left) do
+    with [_ | _] = offsets <- offset(type),
+         %MapSet{} = coordinates <- add_coordinates(offsets, upper_left) do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+    else
+      error -> error
+    end
+  end
+
+  defp offset(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
+  defp offset(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
+  defp offset(:dot), do: [0, 0]
+  defp offset(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
+  defp offset(:s_shape), do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
+  defp offset(_), do: [:error, :invalid_island_type]
+
+  # enumerates over the list of offsets, create a new coordinate for each one, and put them all into the same set. It takes an enumerable, a starting value for an accumulator, and a function to apply to each enumerated value. For us, those three arguments will be the list of offsets, a new MapSet, and a new function we’ll get to in a minute. must return one of two tagged tuples: either {:cont, some_value} to continue the enumeration, or {:halt, some_value} to end it
+  defp add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+      add_coordinate(acc, upper_left, offset)
+    end)
+  end
+
+  defp add_coordinate(coordinates, %Coordinate{row: row, col: col}, {row_offset, col_offset}) do
+    case Coordinate.new(row + row_offset, col + col_offset) do
+      {:ok, coordinate} ->
+        {:cont, MapSet.put(coordinates, coordinate)}
+
+      {:error, invalid_coordinate} ->
+        {:halt, {:error, :invalid_coordinate}}
+    end
+  end
 end
