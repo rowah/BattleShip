8bc229e16fb132b33fdf602cca7a0d1864b484b0
Rowa <jrowah@gmail.com>
2024-01-01 20:42:43 +0300

    Guesses a coordinate

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -22,6 +22,32 @@ defmodule IslandEngine.Game do
   def set_islands(game, player) when player in @players,
     do: GenServer.call(game, {:set_islands, player})
 
+  def guess_coordinate(game, player, row, col),
+    do: GenServer.call(game, {:guess_coordinate, player, row, col})
+
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state_data) do
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state_data, opponent_key)
+
+    with {:ok, rules} <- Rules.check(state_data.rules, {:guess_coordinate, player_key}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <- Rules.check(rules, {:win_check, win_status}) do
+      state_data
+      |> update_board(opponent_key, opponent_board)
+      |> update_guesses(player_key, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :error ->
+        {:reply, :error, state_data}
+
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state_data}
+    end
+  end
+
   def handle_call({:set_islands, player}, _from, state_data) do
     board = player_board(state_data, player)
 
@@ -83,4 +109,13 @@ defmodule IslandEngine.Game do
     do: Map.update!(state_data, player, fn player -> %{player | board: board} end)
 
   defp player_board(state_data, player), do: Map.get(state_data, player).board
+
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
+
+  defp update_guesses(state_data, player_key, hit_or_miss, coordinate) do
+    update_in(state_data[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
 end
