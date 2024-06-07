f3e1789e402b027582fbb59351fe3132d02a20d6
Rowa <jrowah@gmail.com>
2024-06-08 01:02:45 +0300

    Test starting a new game

--- test/battle_ship/coordinate_test.exs
+++ test/battle_ship/coordinate_test.exs
@@ -3,6 +3,7 @@ defmodule BattleShip.CoordinateTest do
   doctest BattleShip
 
   alias BattleShip.Coordinate
+  # alias BattleShip.CoordinateFixtures
 
   test "create new valid and invalid coordinate" do
     assert Coordinate.new(1, 1) == {:ok, %Coordinate{row: 1, col: 1}}
