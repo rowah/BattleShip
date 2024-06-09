0d595967f2712a832aeab4ef4d0b8a967ce46ca2
Rowa <jrowah@gmail.com>
2024-06-09 10:35:53 +0300

    add styler

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule BattleShip.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
       {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:styler, "~> 1.0.0-rc.0", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
